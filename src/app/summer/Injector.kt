03c519870f03ae91fb49e5e65ec40c75bcf23c35
Anton Keks <anton@codeborne.com>
2021-04-14 09:16:46 +0300

    use injector in main

--- src/app/summer/Injector.kt
+++ src/app/summer/Injector.kt
@@ -5,6 +5,10 @@ import kotlin.reflect.KClass
 class Injector {
   private val instances = mutableMapOf<KClass<*>, Any>()
 
+  fun <T: Any> register(type: KClass<T>, instance: T) {
+    instances[type] = instance
+  }
+
   inline fun <reified T: Any> require() = require(T::class)
 
   fun <T: Any> require(type: KClass<T>): T {
@@ -13,9 +17,5 @@ class Injector {
     val constructor = type.constructors.minByOrNull { it.parameters.size }!!
     val params = constructor.parameters.map { require(it.type.classifier as KClass<*>) }.toTypedArray()
     return constructor.call(*params)
-  }
-
-  fun <T: Any> register(type: KClass<T>, instance: T) {
-    instances[type] = instance
   }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,12 @@
-import app.net.ApiGateway
 import app.net.RepositoryRepository
+import app.summer.Injector
 import java.net.http.HttpClient
 import java.net.http.HttpClient.Redirect.NORMAL
 
 suspend fun main(args: Array<String>) {
-  val http = HttpClient.newBuilder().followRedirects(NORMAL).build()
-  val api = ApiGateway(http)
-  val repo = RepositoryRepository(api)
+  val injector = Injector().apply {
+    register(HttpClient::class, HttpClient.newBuilder().followRedirects(NORMAL).build())
+  }
+  val repo: RepositoryRepository = injector.require()
   println(repo.search("jpoint"))
 }
