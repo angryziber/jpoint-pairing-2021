c8e0f835c8e7c4d311c52dc2d8b59538cbbdbe40
Anton Keks <anton@codeborne.com>
2021-04-14 09:13:15 +0300

    make code async using coroutines

--- src/app/net/ApiGateway.kt
+++ src/app/net/ApiGateway.kt
@@ -1,13 +1,11 @@
 package app.net
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.coroutines.future.await
 import java.net.URI
 import java.net.http.HttpClient
-import java.net.http.HttpClient.Redirect.NORMAL
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers.ofInputStream
 
@@ -17,8 +15,8 @@ class ApiGateway(private val http: HttpClient) {
     val json = jacksonObjectMapper().setPropertyNamingStrategy(SnakeCaseStrategy()).configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
   }
 
-  fun getRaw(path: String) = http.send(request(path).GET().build(), ofInputStream()).body()
-  inline fun <reified T> get(path: String): T = getRaw(path).use { json.readValue(it, T::class.java) }
+  suspend fun getRaw(path: String) = http.sendAsync(request(path).GET().build(), ofInputStream()).await().body()
+  suspend inline fun <reified T> get(path: String): T = getRaw(path).use { json.readValue(it, T::class.java) }
 
   private fun request(path: String) = HttpRequest.newBuilder(URI(baseUrl + path)).header("Accept", "application/vnd.github.v3+json")
 }
--- src/main.kt
+++ src/main.kt
@@ -3,7 +3,7 @@ import app.net.RepositoryRepository
 import java.net.http.HttpClient
 import java.net.http.HttpClient.Redirect.NORMAL
 
-fun main(args: Array<String>) {
+suspend fun main(args: Array<String>) {
   val http = HttpClient.newBuilder().followRedirects(NORMAL).build()
   val api = ApiGateway(http)
   val repo = RepositoryRepository(api)
