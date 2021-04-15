12bb171fda806dd5b92919eacf936af3c507bb12
Anton Keks <anton@codeborne.com>
2021-04-15 10:55:36 +0300

    use primaryConstructor from kotlin-reflect

--- src/app/summer/Injector.kt
+++ src/app/summer/Injector.kt
@@ -1,6 +1,7 @@
 package app.summer
 
 import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 class Injector {
   private val instances = mutableMapOf<KClass<*>, Any>()
@@ -14,7 +15,7 @@ class Injector {
   fun <T: Any> require(type: KClass<T>): T {
     val instance = instances[type] as T?
     if (instance != null) return instance
-    val constructor = type.constructors.minByOrNull { it.parameters.size }!!
+    val constructor = type.primaryConstructor ?: type.constructors.minByOrNull { it.parameters.size } ?: error("$type has no available constructors")
     val params = constructor.parameters.map { require(it.type.classifier as KClass<*>) }.toTypedArray()
     return constructor.call(*params)
   }
